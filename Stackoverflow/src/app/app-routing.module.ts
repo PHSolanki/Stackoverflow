79e89050a385d35f44eca5a955194863d0634387
PHSolanki <priyanshusolanki23@gmail.com>
2023-04-12 12:28:45 +0530

    Level-1 Evaluation Test

---
+++ Stackoverflow/src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ErrorComponent } from './error/error.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+},{
+    path: 'user',
+    loadChildren: () => import ('./user/user.module').then(m => m.UserModule)
+},
+// {
+//     path: '**',
+//     component: ErrorComponent
+// }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
