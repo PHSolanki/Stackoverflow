dd93e5b8f9848454e3bf7cd4f7a6700c93e8780d
PHSolanki <priyanshusolanki23@gmail.com>
2023-04-12 12:26:02 +0530

    final update

--- Stackoverflow/src/app/user/login/login.component.ts
+++ Stackoverflow/src/app/user/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent {
     this.user_login=new FormGroup({
 
       email: new FormControl('', [Validators.required,Validators.email]),
-      password: new FormControl('', [Validators.required,Validators.minLength(8)]),
+      password: new FormControl('', [Validators.required,Validators.minLength(8),Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]),
 
     });
 
--- Stackoverflow/src/app/user/sign-up/sign-up.component.ts
+++ Stackoverflow/src/app/user/sign-up/sign-up.component.ts
@@ -25,7 +25,7 @@ export class SignUpComponent {
 
       displayname:new FormControl ("",[Validators.required,Validators.minLength(2)]),
       email:new FormControl ("",[Validators.required,Validators.email]),
-      password: new FormControl('', [Validators.required,Validators.minLength(8)]) 
+      password: new FormControl('', [Validators.required,Validators.minLength(8),Validators.pattern('(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&].{8,}')]) 
 
     })
   }
