d5cc401be053c52d371ea733bd90ac473d37bcda
PHSolanki <priyanshusolanki23@gmail.com>
2023-04-12 13:07:47 +0530

    update

--- Stackoverflow/src/app/user/user-routing.module.ts
+++ Stackoverflow/src/app/user/user-routing.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from '../Guards/auth.guard';
 import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 
 const routes: Routes = [
 
-{path: 'login' , component:LoginComponent},
-{path: 'signup' , component:SignUpComponent},
-{path: 'user-profile' , component:UserProfileComponent}
+{path: 'login' , component:LoginComponent ,canActivate:[AuthGuard]},
+{path: 'signup' , component:SignUpComponent ,canActivate:[AuthGuard]},
+{path: 'user-profile' , component:UserProfileComponent , canActivate:[AuthGuard]}
 
 ];
 
